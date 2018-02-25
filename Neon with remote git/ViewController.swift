3a6131efb4101ab9f228a4b8e9e74fb5bd9d7d4e
a8sx <todorovicdraganetf@gmail.com>
2018-02-25 13:20:52 +0100

    initial commit

---
+++ Neon with remote git/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Neon with remote git
+//
+//  Created by a8sx on 2/25/18.
+//  Copyright © 2018 a8sx. All rights reserved.
+//
+
+import UIKit
+import Neon
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'Neon with remote git' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Neon with remote git
+  pod 'Neon'
+end
